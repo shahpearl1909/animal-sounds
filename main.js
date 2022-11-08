02b1d9f8078c98b4df1cc14c29d57495846a14bd
shahpearl1909 <87839222+shahpearl1909@users.noreply.github.com>
2022-11-07 18:10:34 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function classifier(){
+    navigator.mediaDevices.getUserMedia({audio:true });
+    classifer=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/VLgqEbvkm/model.json", modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResult);
+}
